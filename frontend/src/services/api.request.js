ac1e6abf4488476589ee43c3c92bafdad3cbbfd9
Promise Morka <onyekamorka06@gmail.com>
2022-10-25 09:41:43 -0400

    WIP

--- frontend/src/services/api.request.js
+++ frontend/src/services/api.request.js
@@ -6,101 +6,101 @@ import { API_URL, REFRESH_ENDPOINT } from "./auth.constants";
  * Create an Axios Client with defaults
  */
 const client = axios.create({
-  baseURL: API_URL,
+	baseURL: API_URL,
 });
 
 client.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const originalRequest = error.config;
+	(response) => response,
+	(error) => {
+		const originalRequest = error.config;
 
-    // Prevent infinite loops
-    if (
-      error.response.status === 401 &&
-      originalRequest.url === API_URL + REFRESH_ENDPOINT
-    ) {
-      window.location.href = "/login/";
-      return Promise.reject(error);
-    }
+		// Prevent infinite loops
+		if (
+			error.response.status === 401 &&
+			originalRequest.url === API_URL + REFRESH_ENDPOINT
+		) {
+			window.location.href = "/login/";
+			return Promise.reject(error);
+		}
 
-    if (
-      error.response.data.code === "token_not_valid" &&
-      error.response.status === 401 &&
-      error.response.statusText === "Unauthorized"
-    ) {
-      const user = localStorage.getItem("user");
+		if (
+			error.response.data.code === "token_not_valid" &&
+			error.response.status === 401 &&
+			error.response.statusText === "Unauthorized"
+		) {
+			const user = localStorage.getItem("user");
 
-      if (user) {
-        const tokenParts = JSON.parse(atob(user.refresh.split(".")[1]));
+			if (user) {
+				const tokenParts = JSON.parse(atob(user.refresh.split(".")[1]));
 
-        // exp date in token is expressed in seconds, while now() returns milliseconds:
-        const now = Math.ceil(Date.now() / 1000);
-        console.log(tokenParts.exp);
+				// exp date in token is expressed in seconds, while now() returns milliseconds:
+				const now = Math.ceil(Date.now() / 1000);
+				console.log(tokenParts.exp);
 
-        if (tokenParts.exp > now) {
-          return client
-            .post(REFRESH_ENDPOINT, { refresh: user.refresh })
-            .then((response) => {
-              localStorage.setItem("user", response.data);
+				if (tokenParts.exp > now) {
+					return client
+						.post(REFRESH_ENDPOINT, { refresh: user.refresh })
+						.then((response) => {
+							localStorage.setItem("user", response.data);
 
-              client.defaults.headers["Authorization"] =
-                "Bearer " + response.data.access;
-              originalRequest.headers["Authorization"] =
-                "Bearer " + response.data.access;
+							client.defaults.headers["Authorization"] =
+								"Bearer " + response.data.access;
+							originalRequest.headers["Authorization"] =
+								"Bearer " + response.data.access;
 
-              return client(originalRequest);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        } else {
-          console.log("Refresh token is expired", tokenParts.exp, now);
-          window.location.href = "/login/";
-        }
-      } else {
-        console.log("Refresh token not available.");
-        window.location.href = "/login/";
-      }
-    }
+							return client(originalRequest);
+						})
+						.catch((err) => {
+							console.log(err);
+						});
+				} else {
+					console.log("Refresh token is expired", tokenParts.exp, now);
+					window.location.href = "/login/";
+				}
+			} else {
+				console.log("Refresh token not available.");
+				window.location.href = "/login/";
+			}
+		}
 
-    // specific error handling done elsewhere
-    return Promise.reject(error);
-  }
+		// specific error handling done elsewhere
+		return Promise.reject(error);
+	}
 );
 
 /**
  * Request Wrapper with default success/error actions
  */
 const request = async (opts) => {
-  let options = {
-    ...opts,
-    headers: authHeader(),
-  };
+	let options = {
+		...opts,
+		headers: authHeader(),
+	};
 
-  const onSuccess = (response) => {
-    console.debug("Request Successful!", response);
-    return response;
-  };
+	const onSuccess = (response) => {
+		console.debug("Request Successful!", response);
+		return response;
+	};
 
-  const onError = (error) => {
-    console.error("Request Failed:", error.config);
+	const onError = (error) => {
+		console.error("Request Failed:", error.config);
 
-    if (error.response) {
-      // Request was made but server responded with something
-      // other than 2xx
-      console.error("Status:", error.response.status);
-      console.error("Data:", error.response.data);
-      console.error("Headers:", error.response.headers);
-    } else {
-      // Something else happened while setting up the request
-      // triggered the error
-      console.error("Error Message:", error.message);
-    }
+		if (error.response) {
+			// Request was made but server responded with something
+			// other than 2xx
+			console.error("Status:", error.response.status);
+			console.error("Data:", error.response.data);
+			console.error("Headers:", error.response.headers);
+		} else {
+			// Something else happened while setting up the request
+			// triggered the error
+			console.error("Error Message:", error.message);
+		}
 
-    return Promise.reject(error.response || error.message);
-  };
+		return Promise.reject(error.response || error.message);
+	};
 
-  return await client(options).then(onSuccess).catch(onError);
+	return await client(options).then(onSuccess).catch(onError);
 };
 
 export default request;
--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,83 +1,74 @@
 import {
-  LOGIN_ENDPOINT,
-  REFRESH_ENDPOINT,
-  REGISTER_ENDPOINT,
+	LOGIN_ENDPOINT,
+	REFRESH_ENDPOINT,
+	REGISTER_ENDPOINT,
 } from "./auth.constants";
 
 import request from "./api.request";
 
 class AuthService {
-  constructor() {
-    this.login = this.login.bind(this);
-  }
+	constructor() {
+		this.login = this.login.bind(this);
+	}
 
-  async login(username, password) {
-    let resp = await request({
-      url: LOGIN_ENDPOINT,
-      method: "POST",
-      data: {
-        username,
-        password,
-      },
-    });
+	async login(username, password) {
+		let resp = await request({
+			url: LOGIN_ENDPOINT,
+			method: "POST",
+			data: {
+				username,
+				password,
+			},
+		});
 
-    if (resp.data.access) {
-      return this.setToken(resp);
-    }
-  }
+		if (resp.data.access) {
+			return this.setToken(resp);
+		}
+	}
 
-  logout() {
-    localStorage.removeItem("user");
-  }
+	logout() {
+		localStorage.removeItem("user");
+	}
 
-  async register({
-    username,
-    email,
-    password,
-    firstName,
-    lastName,
-    githublink,
-    linkedinlink,
-    isAdmin,
-  }) {
-    await request({
-      url: REGISTER_ENDPOINT,
-      method: "POST",
-      data: {
-        username,
-        email,
-        password,
-        first_name: firstName,
-        last_name: lastName,
-        githublink,
-        linkedinlink,
-      },
-    })
-      .catch((err) => {
-        console.log(err);
-      })
-      .then(() => {
-        this.login(username, password);
-      });
-  }
+	async register({ username, email, password, firstName, lastName, isAdmin }) {
+		await request({
+			url: REGISTER_ENDPOINT,
+			method: "POST",
+			data: {
+				username,
+				email,
+				password,
+				firstName,
+				lastName,
+				isAdmin,
+			},
+		})
+			.catch((err) => {
+				console.log(err);
+			})
+			.then(() => {
+				this.login(username, password);
+			});
+	}
 
-  setToken(resp) {
-    localStorage.setItem("user", JSON.stringify(resp.data));
-    return resp.data;
-  }
+	setToken(resp) {
+		console.log("FROM SET TOKEN", resp.data.token);
+		localStorage.setItem("user", JSON.stringify(resp.data));
+		return resp.data;
+	}
 
-  async refreshToken() {
-    let user = JSON.parse(localStorage.getItem("user"));
-    if (user.refresh) {
-      return await request({
-        url: REFRESH_ENDPOINT,
-        method: "POST",
-        data: {
-          refresh: user.refresh,
-        },
-      });
-    }
-  }
+	async refreshToken() {
+		let user = JSON.parse(localStorage.getItem("user"));
+		if (user.refresh) {
+			return await request({
+				url: REFRESH_ENDPOINT,
+				method: "POST",
+				data: {
+					refresh: user.refresh,
+				},
+			});
+		}
+	}
 }
 
 export default new AuthService();
