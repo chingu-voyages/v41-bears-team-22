6219c2973c7fb8160d8f8f38244d43e2e058adbb
Promise Morka <onyekamorka06@gmail.com>
2022-10-25 09:07:21 -0400

    Updated username

--- backend/fasttrack-seed.sql
+++ backend/fasttrack-seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (username, password, first_name, last_name, email, is_admin)
 VALUES(
-        'testUer',
+        'testUser',
         '$2b$12$hBTHK/V8T1v6W2k3f5ku6OyX8/6vFfv1t/qkvCyVBGxhGMbRH74W2',
         'tester_firstName',
         'tester_LastName',
@@ -16,13 +16,13 @@ VALUES(
 --         '24012334566');
 
 INSERT INTO applications (username, role, company_name, jobpostlink, location, dateofapplication, status)
-VALUES ('testUer',
+VALUES ('testUser',
         'Software Engineer',
         'ABC Tech',
         'https://abctech.com/techjobs',
         'Washington DC',
         '2022-10-01',
-        'Applied'),('Promisethedev',
+        'Applied'),('testUser',
         'Full Stack Software Engineer',
         'DEF Tech',
         'https://deftech.com/techjobs',
